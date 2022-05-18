f5dba43e36c8b8e2628df0e71b11ae3cbb437d83
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2022-05-18 21:54:33 +0300

    feat: add api-related types

--- src/types/api/backend.ts
+++ src/types/api/backend.ts
@@ -24,3 +24,44 @@ export type LoginRequest = {
 export type LoginResponse = {
   token: string;
 };
+
+export type BoardRequest = {
+  title: string;
+};
+
+export type BoardResposne = {
+  id: string;
+  title: string;
+  columns?: ColumnResponse[];
+};
+
+export type ColumnRequest = {
+  title: string;
+  order: number;
+};
+
+export type ColumnResponse = {
+  id: string;
+  title: string;
+  order: number;
+  tasks?: TaskResponse[];
+};
+
+export type TaskRequest = {
+  title: string;
+  order: number;
+  description: string;
+  userId: string;
+  boardId?: string;
+  columnId?: string;
+};
+
+export type TaskResponse = {
+  id: string;
+  title: string;
+  order: number;
+  description: string;
+  userId: string;
+  boardId: string;
+  columnId: string;
+};
