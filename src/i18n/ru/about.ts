de0b596b49a90b75d6ff23f4e13af6a33958e8bb
alexpataman[mac] <alex.pataman@gmail.com>
2022-05-21 19:58:15 +0200

    fix: typo

--- src/i18n/ru/about.ts
+++ src/i18n/ru/about.ts
@@ -1,7 +1,7 @@
 // TODO: set proper contribution descriptions
 export const ABOUT = {
   ABOUT_TITLE: 'Команда',
-  ALEXANDER_PATAMAN_NAME: 'Алесандр Патаман',
+  ALEXANDER_PATAMAN_NAME: 'Александр Патаман',
   ALEXANDER_PATAMAN_CONTRIBUTION: 'API, общие компоненты',
   DANIYAR_TELENGUTOV_NAME: 'Данияр Теленгутов',
   DANIYAR_TELENGUTOV_CONTRIBUTION: 'Управления досками',
