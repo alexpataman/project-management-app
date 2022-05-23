d9f8628df6ff5a31b9fa021822110ce6c6020c04
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2022-05-23 11:39:33 +0300

    feat: add board slice to the redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
 
-import { BOARDS_SLICE_NAME, USER_SLICE_NAME } from '../constants/store';
+import { BOARDS_SLICE_NAME, BOARD_SLICE_NAME, USER_SLICE_NAME } from '../constants/store';
+import { boardReducer } from './board/board.slice';
 import { boardsReducer } from './boards/boards.slice';
 import { userReducer } from './user/user.slice';
 
@@ -8,6 +9,7 @@ export const store = configureStore({
   reducer: {
     [USER_SLICE_NAME]: userReducer,
     [BOARDS_SLICE_NAME]: boardsReducer,
+    [BOARD_SLICE_NAME]: boardReducer,
   },
 });
 
