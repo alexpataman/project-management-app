a22849ff84ab94142a8cbeaf9279175d03cfdf71
saratovkin <svrvtwkxxn@gmail.com>
2022-05-30 15:48:55 +0300

    fix: update translation files

--- src/i18n/ru/board.ts
+++ src/i18n/ru/board.ts
@@ -1,9 +1,9 @@
 export const BOARD = {
   BOARD_ADD_CARD: 'Добавить карточку',
-  BOARD_MODAL_ADD_COLUMN: 'Добавить колонку',
-  BOARD_MODAL_ADD_TASK: 'Добавить карточку',
+  BOARD_MODAL_FORM_COLUMN: 'Добавить колонку',
+  BOARD_MODAL_FORM_TASK: 'Добавить карточку',
+  BOARD_MODAL_FORM_EDIT: 'Изменить карточку',
   BOARD_MODAL_DELETE_CARD: 'Удалить карточку',
-  BOARD_MODAL_CHANGE_CARD: 'Изменить карточку',
   BOARD_MODAL_NAME: 'Имя',
   BOARD_MODAL_DESCRIPTION: 'Описание',
   BOARD_MODAL_CANCEL: 'Отменить',
